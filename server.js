2cc4dc32eb4fccbe474e33b653a8e8c5ad0f3402
Emerick <pacaudemerick@gmail.com>
2026-01-21 12:37:57 +0100

    Ajout d'une page

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
-// Node.js - Express sample application
+// // Node.js - Express sample application
 
-var express = require('express')
-var app = express()
+// var express = require('express')
+// var app = express()
+
+// app.get('/', function (req, res) {
+//   res.send(`Bonjour, comment ça vas ?!`)
+// })
+
+// var server = app.listen(process.env.PORT || 3000, function () {
+//   var host = server.address().address
+//   var port = server.address().port
+//   console.log('App listening at https://%s:%s', host, port)
+// })
+
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.get('/acceuil', function(req, res) {
+    res.sendFile(path.join(__dirname, 'src', 'acceuil.html'));
+});
 
 app.get('/', function (req, res) {
-  res.send(`Bonjour, comment ça vas ?!`)
-})
+  res.redirect('/acceuil');
+});
 
 var server = app.listen(process.env.PORT || 3000, function () {
-  var host = server.address().address
-  var port = server.address().port
-  console.log('App listening at https://%s:%s', host, port)
-})
+  console.log('Serveur lancé sur le port ' + server.address().port);
+});
