ee98d0ca7e676625c3eb1db0b3712952d8d89677
Emerick <pacaudemerick@gmail.com>
2026-01-21 13:38:48 +0100

    Je retante textContent

--- static/script/NameFormulaire.js
+++ static/script/NameFormulaire.js
@@ -14,6 +14,6 @@ document.getElementById('name-form').addEventListener('submit', function(e) {
     .then(data => {
         // const bloque = document.getElementById("bloqueRéponse");
         // bloque.innerHTML = '';
-        document.getElementById('reponse').innerHTML += `<p>${data}</p>`;
+        document.getElementById('reponse').textContent += `<p>${data}</p>`;
     });
 });
