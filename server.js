7bdef30fab17a3d72092a07bb8dfb28b399c00d4
Emerick <pacaudemerick@gmail.com>
2026-01-21 12:52:09 +0100

    Ajout de la demande du nom et prénom pour le formulaire

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.use(express.urlencoded({ extended: true })); // Permet de lire les données POST
-app.use(express.static('src')); // Pour les fichiers CSS/JS
+app.use(express.urlencoded({ extended: true })); // POST
+app.use(express.static('src')); // CSS/JS
 
 app.get('/accueil', function(req, res) {
-    res.sendFile(path.join(__dirname, 'src', 'accueil.html'));
+  res.sendFile(path.join(__dirname, 'src', 'accueil.html'));
 });
 
 app.get('/', function (req, res) {
@@ -19,18 +19,11 @@ var server = app.listen(process.env.PORT || 3000, function () {
 
 //------
 
-
-
-// Traitement du formulaire
 app.post('/submit-form', function (req, res) {
-  // Les données se trouvent dans req.body
-  var nom = req.body.user_name;
-  var message = req.body.user_message;
+  var fisrtName = req.body.user_fistName;
+  var lastName = req.body.user_lastName;
   
-  console.log("Nom reçu : " + nom);
-  console.log("Message reçu : " + message);
-
-  res.send(`Merci ${nom}, votre message a bien été reçu !`);
+  res.send(`Bonjour ${fisrtName} ${lastName} !`);
 });
 
 app.listen(3000, () => console.log('Serveur actif sur le port 3000'));
