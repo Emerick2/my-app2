22c28a1b2a17b6bb6900a47cc9b3bff78f4eb29f
Emerick <pacaudemerick@gmail.com>
2026-01-21 12:09:01 +0100

    Ajout du serveur

--- server.js
+++ server.js
@@ -4,17 +4,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send(`<!DOCTYPE HTML>
-        <html lang="fr">
-        <head>
-            <meta charset="UTF-8">
-            <title>Page d'accueil</title>
-        </head>
-        <body>
-            <h1>Bienvenue</h1>
-            <p>Ceci est ma page servie par Express.</p>
-        </body>
-        </html>`)
+  res.send(`Bonjour, comment ça vas ?!`)
 })
 
 var server = app.listen(process.env.PORT || 3000, function () {
