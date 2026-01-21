9f8cf1525e0d0a71b71d53e9b35a0270b8770287
Emerick <pacaudemerick@gmail.com>
2026-01-21 13:31:15 +0100

    [Correction] si l'utilisateur clique plusieurs fois sur le bouton, la réponce précédante disparais pour n'e n laiser qu'une unique version

---
+++ static/script/NameFormulaire.js
@@ -0,0 +1,19 @@
+document.getElementById('name-form').addEventListener('submit', function(e) {
+    e.preventDefault(); 
+
+    const formData = new URLSearchParams(new FormData(this));
+
+    fetch('/name-form', {
+        method: 'POST',
+        body: formData,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    })
+    .then(response => response.text())
+    .then(data => {
+        const bloque = document.getElementById("bloqueRéponse");
+        bloque.innerHTML = '';
+        document.getElementById('reponse').innerHTML += `<p>${data}</p>`;
+    });
+});
