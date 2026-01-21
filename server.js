5074ecaf55c60e088f748b7d814cf35b9232ca2f
Emerick <pacaudemerick@gmail.com>
2026-01-21 12:43:59 +0100

    Ajout d'une page

--- server.js
+++ server.js
@@ -1,21 +1,9 @@
-// // Node.js - Express sample application
-
-// var express = require('express')
-// var app = express()
-
-// app.get('/', function (req, res) {
-//   res.send(`Bonjour, comment ça vas ?!`)
-// })
-
-// var server = app.listen(process.env.PORT || 3000, function () {
-//   var host = server.address().address
-//   var port = server.address().port
-//   console.log('App listening at https://%s:%s', host, port)
-// })
-
 var express = require('express');
 var path = require('path');
 var app = express();
+
+app.use(express.urlencoded({ extended: true })); // Permet de lire les données POST
+app.use(express.static('src')); // Pour les fichiers CSS/JS
 
 app.get('/accueil', function(req, res) {
     res.sendFile(path.join(__dirname, 'src', 'accueil.html'));
@@ -28,3 +16,21 @@ app.get('/', function (req, res) {
 var server = app.listen(process.env.PORT || 3000, function () {
   console.log('Serveur lancé sur le port ' + server.address().port);
 });
+
+//------
+
+
+
+// Traitement du formulaire
+app.post('/submit-form', function (req, res) {
+  // Les données se trouvent dans req.body
+  var nom = req.body.user_name;
+  var message = req.body.user_message;
+  
+  console.log("Nom reçu : " + nom);
+  console.log("Message reçu : " + message);
+
+  res.send(`Merci ${nom}, votre message a bien été reçu !`);
+});
+
+app.listen(3000, () => console.log('Serveur actif sur le port 3000'));
